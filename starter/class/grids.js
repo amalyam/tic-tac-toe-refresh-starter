beb6eb3e2e5f44081374177e71c94072c8b9084f
Amalya Megerman <amalyamegerman@gmail.com>
2022-12-07 18:05:07 -0500

    created horizontalCheck, diagonalCheck, and verticalCheck static methods in TTT

---
+++ starter/class/grids.js
@@ -0,0 +1,22 @@
+class Grids {
+    constructor() {
+    }
+}
+
+let xWins = new Grids(
+    [
+        [['X','X','X'],
+        [' ',' ',' '],
+        [' ',' ',' ']],
+    ],
+    [[' ',' ',' '],
+    ['X','X','X'],
+    [' ',' ',' ']],
+    [[' ',' ',' '],
+     [' ',' ',' '],
+     ['X','X','X']],
+
+
+);
+let oWins = new Grids();
+let ties = new Grids();
--- starter/class/ttt.js
+++ starter/class/ttt.js
@@ -2,14 +2,14 @@ const Screen = require("./screen");
 const Cursor = require("./cursor");
 
 class TTT {
-
   constructor() {
-
     this.playerTurn = "O";
 
-    this.grid = [[' ',' ',' '],
-                 [' ',' ',' '],
-                 [' ',' ',' ']]
+    this.grid = [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ];
 
     this.cursor = new Cursor(3, 3);
 
@@ -18,29 +18,74 @@ class TTT {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', TTT.testCommand);
+    Screen.addCommand("ArrowUp", "move cursor up");
 
     Screen.render();
   }
 
-  // Remove this
-  static testCommand() {
-    console.log("TEST COMMAND");
-  }
-
   static checkWin(grid) {
+    let emptyGrid = [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ];
 
+    if (grid === emptyGrid) {
+      // Return false if the game has not ended
+      return false;
+    } else {
+      let horizontalWin = horizontalCheck(grid);
+      if (horizontalWin) {
+        //if horizontalCheck did not return a falsey,
+        //return the letter in the first place in the returned array
+        return horizontalWin[0][0];
+
+        //else, check for a diagonal win
+      } else {
+        //if no horizontal or diagonal win, check for vertical win
+      }
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
-    // Return false if the game has not ended
+    }
+  }
 
+  
+
+  static horizontalCheck(grid) {
+    return grid.find((row) =>
+    row.every((space) => row[0] === space && space != " ")
+  }
+
+  static diagonalCheck(grid) {
+    if (grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]) {
+      return grid[0][0];
+    } else if (grid[0][2] === grid[1][1] && grid[0][2] === grid[2][0]) {
+      return grid[0][2];
+  }
+}
+
+  static verticalCheck(grid) {
+    let winningLetter;
+    for (let h = 0; h < grid.length; h++) {
+      winningLetter = grid[0][h];
+      let count = 0;
+      for (let v = 1; v < grid[0].length; v++) {
+        if (winningLetter === grid[v][h] && grid[v][h] !== " ") {
+          count++;
+          if (count === 2) {
+            return winningLetter;
+          }
+        }
+      }
+    }
+    return false;
   }
 
   static endGame(winner) {
-    if (winner === 'O' || winner === 'X') {
+    if (winner === "O" || winner === "X") {
       Screen.setMessage(`Player ${winner} wins!`);
-    } else if (winner === 'T') {
+    } else if (winner === "T") {
       Screen.setMessage(`Tie game!`);
     } else {
       Screen.setMessage(`Game Over`);
@@ -48,7 +93,6 @@ class TTT {
     Screen.render();
     Screen.quit();
   }
-
 }
 
 module.exports = TTT;
