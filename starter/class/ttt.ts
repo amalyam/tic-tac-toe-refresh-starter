f5b7fa3fe4ebe88e1a6d8af0dc2eba38fdec563c
Max Ainatchi <maxwell.ainatchi@gmail.com>
2022-12-10 12:45:53 -0500

    Fix bugs with game

--- starter/class/ttt.ts
+++ starter/class/ttt.ts
@@ -3,17 +3,17 @@ import Cursor from "./cursor";
 
 export default class TTT {
   public playerTurn: Player = "O";
-  public grid: GridSpace[][] = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "],
-  ];
 
   public cursor = new Cursor(3, 3);
   public space: GridSpace;
 
   constructor() {
-    this.space = this.grid[this.cursor.row][this.cursor.col];
+    Screen.grid = [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ];
+    this.space = Screen.grid[this.cursor.row][this.cursor.col];
 
     // Initialize a 3x3 tic-tac-toe grid
     Screen.initialize(3, 3);
@@ -46,15 +46,13 @@ export default class TTT {
 
     Screen.addCommand("r", "reset the game", TTT.resetGame.bind(this));
 
-    Screen.addCommand("q", "quit the game", Screen.quit);
-
     Screen.render();
   }
 
   placeMove() {
     //what is bind doing exactly in addCommand above?
     Screen.render();
-    //why couldn't I do below with this.grid
+    //why couldn't I do below with Screen.grid
     if (Screen.grid[this.cursor.row][this.cursor.col] === " ") {
       Screen.setGrid(this.cursor.row, this.cursor.col, this.playerTurn);
       if (this.playerTurn === "O") {
@@ -79,10 +77,10 @@ export default class TTT {
     // Returns 'O' if player O wins
     // Returns 'T' if the game is a tie
     // Returns false if the game has not ended
-    const emptyGrid = this.grid.every((row) =>
+    const emptyGrid = Screen.grid.every((row) =>
       row.every((space) => space === " ")
     );
-    const fullGrid = this.grid.every((row) =>
+    const fullGrid = Screen.grid.every((row) =>
       row.every((space) => space !== " ")
     );
 
@@ -90,6 +88,7 @@ export default class TTT {
       // Return false if the game has not ended
       return false;
     }
+
     return (
       this.horizontalCheck() ||
       this.diagonalCheck() ||
@@ -98,40 +97,36 @@ export default class TTT {
     );
   }
 
-  horizontalCheck(): GridSpace | false {
-    let win = this.grid.find((row) =>
+  horizontalCheck(): Player | undefined {
+    let win = Screen.grid.find((row) =>
       row.every((space) => row[0] === space && space != " ")
-    );
-    if (win) {
-      return win[0];
-    } else {
-      return false;
-    }
+    ) as Player[] | undefined;
+    return win?.[0];
   }
 
   diagonalCheck(): Player | undefined {
     if (
-      this.grid[0][0] !== " " &&
-      this.grid[0][0] === this.grid[1][1] &&
-      this.grid[0][0] === this.grid[2][2]
+      Screen.grid[0][0] !== " " &&
+      Screen.grid[0][0] === Screen.grid[1][1] &&
+      Screen.grid[0][0] === Screen.grid[2][2]
     ) {
-      return this.grid[0][0];
+      return Screen.grid[0][0];
     } else if (
-      this.grid[0][2] !== " " &&
-      this.grid[0][2] === this.grid[1][1] &&
-      this.grid[0][2] === this.grid[2][0]
+      Screen.grid[0][2] !== " " &&
+      Screen.grid[0][2] === Screen.grid[1][1] &&
+      Screen.grid[0][2] === Screen.grid[2][0]
     ) {
-      return this.grid[0][2];
+      return Screen.grid[0][2];
     }
   }
 
-  verticalCheck(): GridSpace | false {
+  verticalCheck(): Player | undefined {
     let winningLetter;
-    for (let h = 0; h < this.grid.length; h++) {
-      winningLetter = this.grid[0][h];
+    for (let h = 0; h < Screen.grid.length; h++) {
+      winningLetter = Screen.grid[0][h];
       let count = 0;
-      for (let v = 1; v < this.grid[0].length; v++) {
-        if (winningLetter === this.grid[v][h] && this.grid[v][h] !== " ") {
+      for (let v = 1; v < Screen.grid[0].length; v++) {
+        if (winningLetter === Screen.grid[v][h] && winningLetter !== " ") {
           count++;
           if (count === 2) {
             return winningLetter;
@@ -139,7 +134,6 @@ export default class TTT {
         }
       }
     }
-    return false;
   }
 
   static playAgain() {
